2a8c12783efa9a1f72bc476fb0684f5b25a9eb91
Dmitry Barskov <barskoidim@gmail.com>
2020-02-20 15:09:28 +0300

    Add TimedRobustTest

---
+++ src/test/java/com/github/scfj/decorators/TimedRobustTest.java
@@ -0,0 +1,50 @@
+package com.github.scfj.decorators;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TimedRobustTest {
+    Patient notRobust;
+
+    @Before
+    public void decorateNotRobust() {
+        notRobust = new Patient.Fake(100);
+    }
+
+    @Test
+    public void shouldNotFail() throws IOException {
+        Patient robust = Robust.decorate(notRobust, 2, 100);
+        assertEquals(42, robust.bePatient());
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldFailWhenTimeLimitExceeded() throws IOException {
+        Patient robust = Robust.decorate(notRobust, 2, 40);
+        assertEquals(42, robust.bePatient());
+    }
+
+    interface Patient {
+        int bePatient() throws IOException;
+
+        class Fake implements Patient {
+            private final long respondAfter;
+
+            public Fake(int waitMs) {
+                this.respondAfter = System.currentTimeMillis() + waitMs;
+            }
+
+            @Override
+            public int bePatient() throws IOException {
+                if (System.currentTimeMillis() > respondAfter) {
+                    return 42;
+                } else {
+                    throw new IOException("It's too early. Wait.");
+                }
+            }
+        }
+    }
+}
